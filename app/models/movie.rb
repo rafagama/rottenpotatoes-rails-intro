60651238239e8b9622f8b6f44731307bee96dea5
EC2 Default User <ec2-user@ip-172-31-6-132.us-east-2.compute.internal>
2018-03-01 16:28:02 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,27 @@
 class Movie < ActiveRecord::Base
+    @ratings = ['G','PG','PG-13','NC-17','R']
+
+    def self.ratings
+        return @ratings
+    end
+    
+    def self.is_checked_init
+        @is_checked = Hash.new
+        @ratings.each do |x|
+            @is_checked[x] = true
+        end
+        return @is_checked
+    end
+
+    def self.update_is_checked(keys)
+        @ratings.each do |x|
+            if keys.include?(x)
+                @is_checked[x] = true
+            else
+                @is_checked[x] = false
+            end
+        end
+        return @is_checked
+    end
+
 end
