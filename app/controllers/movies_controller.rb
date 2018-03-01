bcf3347979de8eaead46cfac3085d50024843932
EC2 Default User <ec2-user@ip-172-31-6-132.us-east-2.compute.internal>
2018-03-01 17:42:38 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  __END__
+  end
 
   def index
     @all_ratings = Movie.ratings
